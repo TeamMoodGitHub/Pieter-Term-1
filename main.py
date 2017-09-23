85d187be713c41ef6589ff4fb48ce4c1b8f1cee5
Pjerke <30842400+Pjerke@users.noreply.github.com>
2017-09-23 16:06:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import base64
 #pillow
 
 champions = []
-for i in range(0,500):
+for i in range(0,517):
 	champions.append("Error")
 champions[7] = "LeBlanc"; champions[9] = "Fiddlesticks"; champions[15] = "Sivir"; champions[16] = "Soraka"
 champions[18] = "Tristana"; champions[19] = "Warwick"; champions[22] = "Ashe"; champions[24] = "Jax"
@@ -72,11 +72,14 @@ def basics():
 	idJSON = get_id(base,username,key)
 	ID = str(idJSON['id'])
 	accountID = str(idJSON['accountId'])
-	matchlistJSON = get_matchlist(base,accountID,key)
+	try:
+		matchlistJSON = get_matchlist(base,accountID,key)
+	except KeyError:
+		return render_template("error.html",username=username)
 	#championJSON = get_champions()
 	gameID = []; championID = []; champion = []; duration_s = []
 	duration_t = []; result = []; kills = []; deaths = []
-	assists = []; CS = []; color = []
+	assists = []; CS = []; color = []; mode = [];
 	for i in range(0,20):
 		gameID.append(str(matchlistJSON['matches'][i]['gameId']))
 		championID.append(matchlistJSON['matches'][i]['champion'])
@@ -113,17 +116,13 @@ def basics():
 		deaths.append(str(matchJSON['participants'][participantID-1]['stats']['deaths']))
 		assists.append(str(matchJSON['participants'][participantID-1]['stats']['assists']))
 		CS.append(str(matchJSON['participants'][participantID-1]['stats']['totalMinionsKilled']))
+		mode.append(matchJSON['gameMode'])
 		#KDA.append((kills + "/" + deaths + "/" + assists))
 	with open('data.pickle', 'wb') as f:
-		pickle.dump(base, f, pickle.HIGHEST_PROTOCOL)
-		pickle.dump(key, f, pickle.HIGHEST_PROTOCOL)
-		pickle.dump(regionID, f, pickle.HIGHEST_PROTOCOL)
-		pickle.dump(accountID, f, pickle.HIGHEST_PROTOCOL)
-		pickle.dump(ID, f, pickle.HIGHEST_PROTOCOL)
-		pickle.dump(gameID, f, pickle.HIGHEST_PROTOCOL)
-		pickle.dump(champion, f, pickle.HIGHEST_PROTOCOL)
+		for var in [base, key, regionID, accountID, ID, gameID, champion]:
+			pickle.dump(var, f, pickle.HIGHEST_PROTOCOL)
 	return render_template("main.html",username=username, champion=champion, kills=kills,	\
-		deaths=deaths, assists=assists, CS=CS, duration=duration_t, color=color)
+		deaths=deaths, assists=assists, CS=CS, duration=duration_t, color=color, mode=mode)
 		
 @app.route('/detail<game>')		
 def details(game):
@@ -147,7 +146,9 @@ def details(game):
 	timelineJSON = get_timeline(base,gameID[int(game)-1],key)
 	dframe = timelineJSON['frameInterval']
 	CSdata = []; CSperminute = []; CSgoal = []
-	time = []; deaths = []; check = 0
+	time = []; deaths = []; kills = []; check = 0
+	xPosKill = []; yPosKill = []
+	xPosDeath = []; yPosDeath = []
 	for i in range(0,m+1):
 		j = 0
 		CSdata.append(timelineJSON['frames'][i]['participantFrames'][str(participantID)]['minionsKilled'])
@@ -163,6 +164,12 @@ def details(game):
 				if type == 'CHAMPION_KILL':
 					if timelineJSON['frames'][i]['events'][j]['victimId'] == participantID:
 						deaths.append(timelineJSON['frames'][i]['events'][j]['timestamp'])
+						xPosDeath.append(timelineJSON['frames'][i]['events'][j]['position']['x'])
+						yPosDeath.append(timelineJSON['frames'][i]['events'][j]['position']['y'])
+					elif timelineJSON['frames'][i]['events'][j]['killerId'] == participantID:
+						kills.append(timelineJSON['frames'][i]['events'][j]['timestamp'])
+						xPosKill.append(timelineJSON['frames'][i]['events'][j]['position']['x'])
+						yPosKill.append(timelineJSON['frames'][i]['events'][j]['position']['y'])
 				j += 1
 			except IndexError:
 				break
@@ -185,9 +192,16 @@ def details(game):
 		deaths = np.divide(deaths,dframe)
 		for i in range(0,len(deaths)):
 			if i == 0:
-				plt.axvline(deaths[i], ymin=0, ymax=1, color='tomato', label='Deaths')
+				plt.axvline(deaths[i], ymin=0, ymax=1, color='red', label='Deaths')
 			else:
-				plt.axvline(deaths[i], ymin=0, ymax=1, color='tomato')
+				plt.axvline(deaths[i], ymin=0, ymax=1, color='red')
+	if len(kills) != 0:
+		kills = np.divide(kills,dframe)
+		for i in range(0,len(kills)):
+			if i == 0:
+				plt.axvline(kills[i], ymin=0, ymax=1, color='black', label='Kills')
+			else:
+				plt.axvline(kills[i], ymin=0, ymax=1, color='black')
 	if 'upgrade' in locals():
 		upgrade = np.divide(upgrade,dframe)
 		plt.axvline(upgrade, ymin=0, ymax=1, color='green', label='Hex Core mk-1 Upgrade')
@@ -207,7 +221,25 @@ def details(game):
 	plt.savefig(img, format='png')
 	img.seek(0)
 	plot_url2 = base64.b64encode(img.getvalue()).decode('ascii')
-	return render_template("details.html", plot_url=plot_url, plot_url2=plot_url2)
+	plt.close()
+	img = plt.imread("static/map.PNG")
+	fig, ax = plt.subplots()
+	plt.scatter(xPosDeath, yPosDeath, color='red', label="Death")
+	plt.scatter(xPosKill, yPosKill, color='black', label="Kill")
+	if participantID < 6:
+		plt.scatter(600,600,s=100,color='cyan')
+	else:
+		plt.scatter(14220,14281,s=100,color='tomato')
+	plt.legend(loc='upper left')
+	ax.imshow(img, extent=[0,14820,0,14881])
+	plt.xlim([0,14820])
+	plt.ylim([0,14881])
+	plt.axis('off')
+	img = BytesIO()
+	plt.savefig(img, format='png')
+	img.seek(0)
+	plot_url3 = base64.b64encode(img.getvalue()).decode('ascii')
+	return render_template("details.html", plot_url=plot_url, plot_url2=plot_url2, plot_url3=plot_url3)
 
 	
 if __name__ == "__main__":
