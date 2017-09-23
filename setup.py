4a79d7160de6c88d61b21b3651d043267eadc926
Pjerke <30842400+Pjerke@users.noreply.github.com>
2017-09-23 16:05:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,14 @@ setup(
 	name='CS_Tool',
 	version='1.0',
 	packages=find_packages(),
-	scripts=['app.py'],
-	
-	install_requires=['peppercorn'],
+	install_requires=['flask','matplotlib','numpy','requests','pillow'],
 	python_requires='>=3',
-	
 	author="Pieter",
 	author_email="pieter_gilissen@hotmail.com",
 	description='A CS analysis tool for LOL',
-	url='https://github.com/TeamMoodGitHub/Pieter-Term-1',
 	classifiers=[
 	'Development Status :: 3 - Alpha',
 	'Inteded Audience :: League Players',
 	'Programming Language :: Python :: 3.6',
 	],
-	#packages=['flask','matplotlib','numpy','requests','pickle','pillow']
 	)
